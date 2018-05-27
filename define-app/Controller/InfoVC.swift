2e36f0e19f9b8c58a0678cbc1ef6a07592da6425
DanBonehill <DanBonehill@me.com>
2018-05-27 09:21:17 +0100

    Setup InfoVC & InfoVC Basic UI

--- define-app/Controller/InfoVC.swift
+++ define-app/Controller/InfoVC.swift
@@ -10,7 +10,30 @@ import UIKit
 
 class InfoVC: UIViewController {
 
+    @IBOutlet weak var greetingNameLbl: UILabel!
+    @IBOutlet weak var ageLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var currentWeightLbl: UILabel!
+    @IBOutlet weak var goalWeightLbl: UILabel!
+    @IBOutlet weak var weightToLoseLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUserData()
+    }
+    
+    func setupUserData() {
+        let user = UserService.instance.getUser()
+        let height = user.height.inchesToFt()
+        let weight = user.weight.poundsToSt()
+        let goalWeight = user.goalWeight.poundsToSt()
+        let weightToLose = (user.weight - user.goalWeight).poundsToSt()
+        
+        greetingNameLbl.text = "Hey, \(user.name)"
+        ageLbl.text = String(describing: user.age)
+        heightLbl.text = String(describing: height)
+        currentWeightLbl.text = String(describing: weight)
+        goalWeightLbl.text = String(describing: goalWeight)
+        weightToLoseLbl.text = String(describing: weightToLose)
     }
 }
