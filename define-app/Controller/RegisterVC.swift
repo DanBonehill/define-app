6a11f1c18e36642fd9c1ffeec1eadeedf9e3f3fe
DanBonehill <DanBonehill@me.com>
2018-08-09 19:55:18 +0100

    Improve UI for RegisterVC

--- define-app/Controller/RegisterVC.swift
+++ define-app/Controller/RegisterVC.swift
@@ -18,6 +18,7 @@ class RegisterVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()
     }
     
     @IBAction func signupBtnWasPressed(_ sender: Any) {
